4940ff862710a25eb4e3c5a29dfe56c3530f891a
Bence Mali <bencemali835@gmail.com>
2022-05-18 18:50:57 +0200

    test dir and no system()

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]) {
             test();
         } else { // If there's input, use it as the target
         argc = argc + 0;
-        Makefile mf("makefile");
+        Makefile mf("test/makefile");
         Database db(mf.rules());
         db.build(argv[1]);
         }
@@ -44,12 +44,12 @@ void test() {
 void variable_test() {
     std::cout << "-------------------- Testing Variable interpretation" << std::endl;
     std::cout << "------------ Reading makefile1" << std::endl;
-    Makefile mf1("makefile1");
+    Makefile mf1("test/makefile1");
     Database db1(mf1.rules());
     std::cout << "---- Building target 'target1'" << std::endl;
     db1.build("target1");
     std::cout << "------------ Reading makefile2" << std::endl;
-    Makefile mf2("makefile2");
+    Makefile mf2("test/makefile2");
     Database db2(mf2.rules());
     std::cout << "---- Building target 'target2'" << std::endl;
     db2.build("target2");
@@ -58,7 +58,7 @@ void variable_test() {
 void pattern_test() {
     std::cout << "-------------------- Testing Pattern interpretation" << std::endl;
     std::cout << "------------ Reading makefile3" << std::endl;
-    Makefile mf3("makefile3");
+    Makefile mf3("test/makefile3");
     Database db3(mf3.rules());
     std::cout << "---- Building target 'target.a'" << std::endl;
     db3.build("target.a");
@@ -67,7 +67,7 @@ void pattern_test() {
 void exception_test() {
     std::cout << "-------------------- Testing Exceptions" << std::endl;
     std::cout << "------------ Reading makefile4" << std::endl;
-    Makefile mf4("makefile4");
+    Makefile mf4("test/makefile4");
     Database db4(mf4.rules());
     std::cout << "---- Building target 'prereq'" << std::endl;
     db4.build("prereq");
@@ -81,12 +81,12 @@ void exception_test() {
     std::cout << "---- Building target 'loop1'" << std::endl;
     db4.build("loop1");
     std::cout << "------------ Reading makefile5" << std::endl;
-    Makefile mf5("makefile5");
+    Makefile mf5("test/makefile5");
     Database db5(mf5.rules());
     std::cout << "---- Building target 'looproot'" << std::endl;
     db5.build("looproot");
     std::cout << "------------ Reading makefile6" << std::endl;
-    Makefile mf6("makefile6");
+    Makefile mf6("test/makefile6");
     Database db6(mf6.rules());
     db6.build("anything");
 }
